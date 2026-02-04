c1227d774f8c09620159c3443bd444b6446037ff
Ruturaj <ruturajnikam1010@gmail.com>
2026-02-04 19:59:54 +0530

    pushed easy of promises

--- promises/easy/promiseRace.js
+++ promises/easy/promiseRace.js
@@ -4,7 +4,15 @@
 // The function should accept an iterable of values that may include Promises or plain values. 
 // It must settle as soon as the first input settles, resolving or rejecting accordingly. 
 // Using Promise.resolve ensures non-promise values are handled correctly.
-function promiseRace(promises) {}
+function promiseRace(promises) {
+    return new Promise((resolve, reject)=>{
+        promises.forEach(item => {
+            Promise.resolve(item)
+            .then(resolve)
+            .catch(reject);
+        });
+    })
+}
 
 module.exports = promiseRace;
 
