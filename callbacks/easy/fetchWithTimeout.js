7c31009947d0063d7de9f392a40bcc98e0107410
Ruturaj <ruturajnikam1010@gmail.com>
2026-02-10 21:13:59 +0530

    completed easy from callbacks

--- callbacks/easy/fetchWithTimeout.js
+++ callbacks/easy/fetchWithTimeout.js
@@ -10,7 +10,25 @@
 
 
 function fetchWithTimeout(url, ms, callback) {
+  let finished = false;
 
+  const timer = setTimeout(() => {
+    if (finished) return;
+    finished = true;
+    callback(new Error("Request Timed Out"));
+  }, ms);
+
+  fetch(url, (err, data) => {
+    if (finished) return;
+    finished = true;
+    clearTimeout(timer);
+
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, data);
+    }
+  });
 }
 
 module.exports = fetchWithTimeout;
