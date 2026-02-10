45ceb7bdbcd9632646b4b188418f5459b314c87b
Ruturaj <ruturajnikam1010@gmail.com>
2026-02-10 21:59:13 +0530

    solved easy of promises

--- promises/easy/promisify.js
+++ promises/easy/promisify.js
@@ -1,17 +1,27 @@
 
-// // Problem Description – Promisify Utility
-// //
-// // You are given a legacy function that uses Node.js callback style:
-// // (err, result) => { ... }.
-// //
-// // Your task is to implement promisify(fn) that converts it into a Promise-based function.
-// //
-// // The returned function must:
-// // 1. Resolve with result if callback gets (null, result)
-// // 2. Reject if callback gets an error
+// Problem Description – Promisify Utility
+//
+// You are given a legacy function that uses Node.js callback style:
+// (err, result) => { ... }.
+//
+// Your task is to implement promisify(fn) that converts it into a Promise-based function.
+//
+// The returned function must:
+// 1. Resolve with result if callback gets (null, result)
+// 2. Reject if callback gets an error
 
 function promisify(fn) {
-
+    return function (...args) {
+        return new Promise((resolve, reject) => {
+            fn(...args, (err, result) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+    };
 }
 
 module.exports = promisify;
